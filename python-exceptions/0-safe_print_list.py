10a0c1911df5c7c5bc704ab771f54498bb18095b
Elizabeth-Ater <e.ater@alustudent.come>
2026-05-05 10:02:25 -0700

    Safe list printing

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,12 +3,12 @@
 def safe_print_list(my_list=[], x=0):
      printed_elts = 0
 
-     try:
-         for i in range(x):
-             print(my_list[i], end="")
-             printed_elts += 1
-     except IndexError:
-         pass
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            printed_elts += 1
+    except IndexError:
+        pass
 
-     print()
-     return printed_elts
+    print()
+    return printed_elts
