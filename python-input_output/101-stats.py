c548baa9b8c358d2edf57ea02efe877acb11d72e
Elizabeth-Ater <e.ater@alustudent.come>
2026-05-12 11:36:06 -0700

    Log parsing

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
 """
-Log parsing module.
+Log parsing script.
 Reads stdin line by line and computes metrics.
 """
 
 import sys
 
 
-def print_stats(total_size, status_codes):
+def print_stats(total_size, status_counts):
     """
     Prints accumulated statistics.
     """
     print("File size: {}".format(total_size))
-    for code in sorted(status_codes):
-        if status_codes[code]:
-            print("{}: {}".format(code, status_codes[code]))
+    for code in sorted(status_counts.keys()):
+        if status_counts[code] > 0:
+            print("{}: {}".format(code, status_counts[code]))
 
 
 def main():
     total_size = 0
     line_count = 0
 
-    status_codes = {
+    status_counts = {
         "200": 0,
         "301": 0,
         "400": 0,
@@ -29,7 +29,7 @@ def main():
         "403": 0,
         "404": 0,
         "405": 0,
-        "500": 0,
+        "500": 0
     }
 
     try:
@@ -45,18 +45,17 @@ def main():
             except (ValueError, IndexError):
                 continue
 
-            if status in status_codes:
-                status_codes[status] += 1
+            if status in status_counts:
+                status_counts[status] += 1
 
             total_size += size
             line_count += 1
 
             if line_count % 10 == 0:
-                print_stats(total_size, status_codes)
+                print_stats(total_size, status_counts)
 
     except KeyboardInterrupt:
-        print_stats(total_size, status_codes)
-        raise
+        print_stats(total_size, status_counts)
 
 
 if __name__ == "__main__":
