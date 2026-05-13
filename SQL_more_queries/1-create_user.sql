347c3f50ce8add2a85b5f7bc9fbfbaf153ad6ea8
Elizabeth-Ater <e.ater@alustudent.come>
2026-05-13 00:48:23 -0700

    Read user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,11 +1,9 @@
--- the mysql command-line tool or a MySQL client application.
+-- Create database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+-- Create user if it doesn't exist with password
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
 
--- Grant all privileges to user_0d_1. 
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost WITH GRANT OPTION;
-
--- Flush privileges to apply the changes.
-FLUSH PRIVILEGES;
-
-EXIT
+-- Grant only SELECT privilege on the database
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
