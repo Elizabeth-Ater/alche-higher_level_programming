4d304eda7419699736d6c19607aa0d0a77c9eadb
Elizabeth-Ater <e.ater@alustudent>
2026-08-05 07:15:39 -0700

    Changes to the file

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 
-const fs = require('fs');
-
-const path = process.argv[2];
-const content = process.argv[3];
-
-try {
-  fs.writeFileSync(path, content, 'utf8');
-} catch (error) {
-  console.log(error);
+class Rectangle {
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
 }
+
+module.exports = Rectangle;
